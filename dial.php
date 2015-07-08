1f9b42421270ed7e8fb8cccaca931351d7d509d0
Pablo Martelletti <pmartelletti@gmail.com>
2015-07-08 20:07:16 -0300

    Add dial reponse.

---
+++ dial.php
@@ -0,0 +1,7 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Say voice="alice">This is a message from CRM Exposed. Please wait while we connect you to Stephen Gill.</Say>
+</Response>

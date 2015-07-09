f9a1882444d2c4307efc73aa2d2d0fc85fe6b31a
Pablo Martelletti <pmartelletti@gmail.com>
2015-07-08 22:32:08 -0300

    Add caller id.

--- dial.php
+++ dial.php
@@ -8,7 +8,7 @@
 ?>
 <Response>
     <Say voice="alice">This is a message from CRM Exposed. Please wait while we connect you to Stephen Gill.</Say>
-    <Dial record="record-from-ringing" callerId="<?php $callerId; ?>">
+    <Dial record="record-from-ringing" callerId="<?php echo $callerId; ?>">
     	<Number><?php echo $number; ?></Number>
     </Dial>
 </Response>
